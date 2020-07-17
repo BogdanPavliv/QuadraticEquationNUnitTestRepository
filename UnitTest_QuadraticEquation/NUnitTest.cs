753f9921e56a42d99855543969d0c6d5c35edc68
Bogdan <bogdan.pavliv@gmail.com>
2020-07-17 14:25:43 +0300

    Added first - DiscriminantTestLessThanZero NUnitTest

--- UnitTest_QuadraticEquation/NUnitTest.cs
+++ UnitTest_QuadraticEquation/NUnitTest.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace UnitTest_QuadraticEquation
 {
+    [TestFixture]
     public class NUnitTest
     {
+        [Test]
+        public void DiscriminantTestLessThanZero()
+        {
+            QuadraticEquation equation = new QuadraticEquation(5, 8, 12);
+            string actualResult = equation.Discriminant();
+            Assert.AreEqual("Рівняння немає коренів", actualResult);
+        }
 
     }
 }
