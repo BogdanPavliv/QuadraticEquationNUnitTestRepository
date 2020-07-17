d0b6acee0597b8bf565c26d1562cee3b4d28cd6a
Bogdan <bogdan.pavliv@gmail.com>
2020-07-17 14:35:38 +0300

    Added fourth - DiscriminantTestMoreThenZero NUnitTest

--- UnitTest_QuadraticEquation/NUnitTest.cs
+++ UnitTest_QuadraticEquation/NUnitTest.cs
@@ -30,6 +30,12 @@ namespace UnitTest_QuadraticEquation
             string actualResult = equation.Discriminant();
             Assert.AreEqual("x = 3", actualResult);
         }
-
+        [Test]
+        public void DiscriminantTestMoreThenZero()
+        {
+            QuadraticEquation equation = new QuadraticEquation(1, -8, 12);
+            string actualResult = equation.Discriminant();
+            Assert.AreEqual("x1 = 6, x2 = 2", actualResult);
+        }
     }
 }
